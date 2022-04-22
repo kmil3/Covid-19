0d8a7443dd073be7c8e3956186e2d09f8b9f8b8b
kmil3_ <you@example.com>
2022-04-21 21:35:58 -0300

    oi

--- index.php
+++ index.php
@@ -18,8 +18,6 @@
             </form>
         </nav>
     </section>
-
-    <video src="midia/capa.mp4" width="auto" height="auto" autoplay="autoplay"  > </video>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </html>
